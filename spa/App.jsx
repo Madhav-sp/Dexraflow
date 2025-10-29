c6e779159a73bdadb353c6006197aa91997aec9f
Madhav <spmadhav04@gmail.com>
2025-10-29 23:10:04 +0530

    little change

--- spa/App.jsx
+++ spa/App.jsx
@@ -1,53 +1,9 @@
-// src/App.jsx
-import Navbar from "./components/Navbar";
-import SectionCard from "./components/SectionCard";
-import Section from "./components/layout/Section";
-// import Divider from "./components/layout/Divider";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero.jsx";
-import RadialTimelineSection from "./components/sections/RadialTimelineSection.jsx";
-import IntegrationsGrid from "./components/Integrations.jsx"
-import ShowcaseSection from "./components/sections/Product.jsx";
-import WeezProductShowcase from "./components/sections/WeezProductShowcase.jsx";
-export default function App() {
+import React from 'react'
+
+const App = () => {
   return (
-    <div className="relative min-h-screen bg-black text-slate-900">
-      <Navbar />
-      <Hero
-          line1="Automate,"
-          line2="Beautifully"
-          kicker="Dexraflow — Where AI meets effortless flow"
-      />
-        <main role="main">
-            <div className="mx-auto max-w-[80rem] px-4">
-                <WeezProductShowcase/>
-            </div>
-            {/*<Divider/>*/}
-            <Section>
-                <RadialTimelineSection/>
-            </Section>
-
-
-
-            {/*<Divider/>*/}
-
-            <Section id="integrations">
-                <SectionCard className={"bg-black"}>
-                    <IntegrationsGrid/>
-                </SectionCard>
-            </Section>
-
-
-            {/*<Divider/>*/}
-
-            <Section id="get-started">
-                <SectionCard className={"bg-black"}>
-                    <ShowcaseSection/>
-                </SectionCard>
-            </Section>
-        </main>
-
-        <Footer/>
-    </div>
-  );
+    <div>HR take me to the hospital...</div>
+  )
 }
+
+export default App
